322e050f0c42a1dc9629bf6961259e716b15618b
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-10-16 11:48:55 +0200

    Primer menú usuario

--- formulario1usr.php
+++ formulario1usr.php
@@ -1,11 +1,12 @@
-<?include ("bloquedeseguridad.php");?>
 <html>
 <head>
 <meta charset="utf-8">
-<title>formulario1adm</title>
+<title>formulario1usr</title>
 </head>
 <body>
-<h1>Esta sección es exclusiva para administradores.</h1>
+<h1>Esta secciÃ³n es exclusiva para usuarios.</h1>
+  <a href="crearincidencia.php">Nueva incidencia</a>
+  <a href="consultaincidenciausr.php">Seguimiento de tus incidencias</a>
 <br>
 <br>
 </body>
