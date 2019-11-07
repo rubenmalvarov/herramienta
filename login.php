c72fc5e273f82d135efb84de8efce20d42eae4f8
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-07 22:11:32 +0100

    Corrección ortográfica

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>Autenticación PHP</title>
+<title>Autenticaci&oacute;n PHP</title>
 </head>
 <body>
 <h1>Formulario de autenticacion</h1>
@@ -8,7 +8,7 @@
 <table border="0">
 <tr><td>Nombre de usuario:</td><td><input name="usuario" size="25" value=""/></td></tr>
 <tr><td>Contrasena:</td><td><input name="contrasena" size="25" type="password"/></td></tr>
-<tr><td/><td><input type="submit" value="Inicio de sesión"/></td></tr>
+<tr><td/><td><input type="submit" value="Inicio de sesi&oacute;n"/></td></tr>
 </table>
 </form>
 </body>
