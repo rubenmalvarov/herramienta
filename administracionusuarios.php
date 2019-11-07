6b35c0fa7ff34b549852f2230c4a089f16af1e78
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-07 22:14:42 +0100

    Create administracionusuarios.php

---
+++ administracionusuarios.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>       
+<meta charset="UTF-8" />       
+<title></title>  
+</head>  
+<body>
+
+<p>Descripci&oacute;n. (¿Cual es el problema?) </p> 
+<br /> <textarea name = descripcion rows = 5 cols = 1></textarea> 
+
+<p>Resumen (Detalla el problema)</p> 
+<br /> <textarea name = descripcion rows = 5 cols = 10></textarea> 
+ 
+<p> <input type = submit value = "Crear" /> <input type = reset value = "Cancelar" /> </p> </form> 
+</form>  
