e8049eb53784d243f1ce46eea05c018dc836255e
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-07 22:19:12 +0100

    Update login.php

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
 <form action="autenticacion.php" method="POST">
 <table border="0">
 <tr><td>Nombre de usuario:</td><td><input name="usuario" size="25" value=""/></td></tr>
-<tr><td>Contrasena:</td><td><input name="contrasena" size="25" type="password"/></td></tr>
+<tr><td>Contraseña:</td><td><input name="contrasena" size="25" type="password"/></td></tr>
 <tr><td/><td><input type="submit" value="Inicio de sesi&oacute;n"/></td></tr>
 </table>
 </form>
