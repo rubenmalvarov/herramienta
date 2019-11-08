16d3aeee3bff7bf4f06a65be817cd101ac5c7208
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-08 12:26:29 +0100

    Update administracionusuarios.php

--- administracionusuarios.php
+++ administracionusuarios.php
@@ -3,16 +3,17 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>       
-<meta charset="UTF-8" />       
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">     
 <title></title>  
 </head>  
 <body>
 
-<p>Descripci&oacute;n. (¿Cual es el problema?) </p> 
-<br /> <textarea name = descripcion rows = 5 cols = 1></textarea> 
-
-<p>Resumen (Detalla el problema)</p> 
-<br /> <textarea name = descripcion rows = 5 cols = 10></textarea> 
+<a href="consultausuario.php">Consulta de usuarios</a>
+<br />
+<a href="creausuario.php">Creaci&oacute;n de usuarios</a>
+<br />
+<a href="eliminausuario.php">Eliminaci&oacute;n usuarios</a>
+<br /> 
  
 <p> <input type = submit value = "Crear" /> <input type = reset value = "Cancelar" /> </p> </form> 
 </form>  
