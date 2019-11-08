7cb6372950e79597e2657b0eff5b798e47781ca4
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-08 12:20:38 +0100

    Create consultausuario.php

---
+++ consultausuario.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>       
+<meta charset="UTF-8" />       
+<title></title>  
+</head>  
+<body>
+<p>Descripci&oacute;n. (¿Cual es el problema?) </p> 
+<br /> <textarea name = descripcion rows = 5 cols = 1></textarea> 
+
+<p>Introduce nombre de usuario</p> 
+<br /> <textarea name = descripcion rows = 5 cols = 10></textarea> 
+ 
