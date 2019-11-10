8913045de30a5bffd2c2dfc3a4be95b0fd09a70f
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-10 19:03:54 +0100

    Delete variables.php

--- variables.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Connection variables
-$dbhost	= "192.168.100.10:3306";	   // IP del servidor
-$dbuser	= "root";		  // Nombre de usuario de la bbdd
-$dbpass	= "clase1234";		     // contraseña de dicho usuario
-$dbname	= "tfg";    // Nombre de la BBDD
-?>
