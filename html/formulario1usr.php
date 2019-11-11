2ea1b04bd293f6421e4551f28d9a90862e849935
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-11 16:54:05 +0100

    Update formulario1usr.php

--- html/formulario1usr.php
+++ html/formulario1usr.php
@@ -1,8 +1,18 @@
-<html>
-<head>
-<meta charset="utf-8">
-<title>formulario1usr</title>
-</head>
+<html lang="es">
+  <head>
+    <title>NukeTrack Administrador</title>
+	 <meta charset="UTF-8">
+
+
+    <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
+	<link rel="stylesheet" href="css/w3.css">
+	<link rel="stylesheet" href="css/estilo.css">
+  </head>
+    
+      <div class="w3-container w3-center">    
+         <!-- Imagen y logo -->
+	<img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
+    </div>
 <body>
 <h1>Esta sección es exclusiva para usuarios.</h1>
   <a href="crearincidencia.php">Nueva incidencia</a>
