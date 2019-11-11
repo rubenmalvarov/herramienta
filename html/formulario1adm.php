ee8237cdaeca969b085e1bf5de7e221e918485d7
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-11 17:06:23 +0100

    Dando un poco de estilo y arreglando enlaces.

--- html/formulario1adm.php
+++ html/formulario1adm.php
@@ -1,4 +1,5 @@
 
+<!doctype html>
 <html lang="es">
   <head>
     <title>NukeTrack Administrador</title>
@@ -8,33 +9,52 @@
     <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
 	<link rel="stylesheet" href="css/w3.css">
 	<link rel="stylesheet" href="css/estilo.css">
-  </head>
+ </head>
+   
+    <!-- Imagen y logo -->
+    <div class="w3-container w3-center">  
+	   <img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
+    </div>
     
-      <div class="w3-container w3-center">    
-         <!-- Imagen y logo -->
-	<img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
+    <br>
+    
+    <div class="w3-container w3-deep-orange w3-center">
+		<h2>Usuario administrador</h2>
+    </div>
+    
+    <br>
+    
+    <div class="w3-container w3-center">
+        <button class="w3-btn w3-deep-orange">
+        <a href="crearincidencia.php">Nueva incidencia</a>
+        </button>
+    </div>
+    
+    <br>
+    
+    <div class="w3-container w3-center">
+        <button class="w3-btn w3-deep-orange">
+        <a href="consultaincidenciaadm.php">Consultar incidencia</a>
+        </button>
+    </div>
+  
+     <br>
+    
+    <div class="w3-container w3-center">
+        <button class="w3-btn w3-deep-orange">
+        <a href="basedeconocimientos.php">Base de conocimientos</a>
+        </button>
+    </div>
+    
+     <br>
+    
+    <div class="w3-container w3-center">
+        <button class="w3-btn w3-deep-orange">
+        <a href="administracionusuarios.php">Administracion de usuarios</a>
+        </button>
     </div>
 
-<h1>Esta sección es exclusiva para administradores.</h1>
-  
-  <br>
-  
-  <div class="w3-container w3-deep-orange w3-center">
-  <a href="crearincidencia.php">Nueva incidencia</a>
-  </div>
-  
-  <br>
-  
-  <div class="w3-container w3-deep-orange w3-center">
-  <a href="consultaincidenciaadm.php">Consultar incidencia</a>
-  </div>
-  
-  <br>
-  <div class="w3-container w3-deep-orange w3-center">
-  <a href="basedeconocimientos.php">Base de conocimientos</a>
-  <br>
-  <a href="administracionusuarios.php">Administracion de usuarios</a>
-<br>
-<br>
+
+    </div>
 
 </html>
