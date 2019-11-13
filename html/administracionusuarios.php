c1737fa750825ac95ceb616da816ad1ddb874ba0
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-13 15:08:22 +0100

    Update administracionusuarios.php

--- html/administracionusuarios.php
+++ html/administracionusuarios.php
@@ -17,3 +17,54 @@
  
 <p> <input type = submit value = "Crear" /> <input type = reset value = "Cancelar" /> </p> </form> 
 </form>  
+
+
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>NukeTrack Administrador</title>
+	 <meta charset="UTF-8">
+    <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
+	<link rel="stylesheet" href="css/w3.css">
+	<link rel="stylesheet" href="css/estilo.css">
+ </head>
+   
+    <!-- Imagen y logo -->
+    <div class="w3-container w3-center">  
+	   <img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
+    </div>
+    
+    <br>
+    
+    <div class="w3-container w3-deep-orange w3-center">
+		<h2>Administración de usuarios</h2>
+    </div>
+    
+    <br>
+    
+    <div class="w3-container w3-center">
+        <button class="w3-btn w3-deep-orange">
+        <a href="consultausuario.php">Consulta de usuarios<</a>
+        </button>
+    </div>
+    
+    <br>
+    
+    <div class="w3-container w3-center">
+        <button class="w3-btn w3-deep-orange">
+        <a href="creausuario.php">Creación de usuarios</a>
+        </button>
+    </div>
+     
+      <br>
+    
+    <div class="w3-container w3-center">
+        <button class="w3-btn w3-deep-orange">
+        <a href="eliminausuario.php">Eliminación usuarios</a>
+        </button>
+    </div>
+  
+     <br>
+    
+</html>
