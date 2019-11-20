ec76714564edb67e4f17c06b40a8eaa747a69009
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-20 17:06:47 +0100

    Update formulario1usr.php

--- html/formulario1usr.php
+++ html/formulario1usr.php
@@ -34,5 +34,9 @@
         <a href="consultaincidenciausr.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Consultar incidencia</a>
         
     </div>
-
+<footer>
+	<div class="w3-container w3-black">
+		<h4>Álvaro Vicente y Rubén Martín TFG</h4>
+	</div>
+</footer>
 </html>
