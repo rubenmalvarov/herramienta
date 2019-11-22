dcc32bf5788e99243fe098d0487b20d2315ab4e5
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-22 18:23:01 +0100

    Update variables.php

--- html/variables.php
+++ html/variables.php
@@ -1,7 +1,7 @@
 <?php
 // Connection variables
 $dbhost	= "192.168.100.10:3306";	   // IP del servidor
-$dbuser	= "root";		  // Nombre de usuario de la bbdd
-$dbpass	= "clase1234";		     // contraseña de dicho usuario
-$dbname	= "tfg";    // Nombre de la BBDD
+$dbidusuario	= "root";		  // Nombre de usuario de la bbdd
+$dbcontrasena	= "clase1234";		     // contraseña de dicho usuario
+$dbnombre	= "tfg";    // Nombre de la BBDD
 ?>
