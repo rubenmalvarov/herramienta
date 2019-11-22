61e89037ef64bc39a0b8ec053dcbbf61c019c1ee
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-22 18:10:35 +0100

    Update crearincidencia.php

--- html/crearincidencia.php
+++ html/crearincidencia.php
@@ -1,10 +1,8 @@
-
 <!doctype html>
 <html lang="es">
   <head>
     <title>NukeTrack Administrador</title>
 	 <meta charset="UTF-8">
-
     <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
 	<link rel="stylesheet" href="css/w3.css">
 	<link rel="stylesheet" href="css/estilo.css">
@@ -17,55 +15,43 @@
     </div>
     
     <!-- Contenedor principal con catégorias y datos de la incidencia a registrar por el usuario -->     
-    <div class="w3-container w3-deep-orange w3-center">
-		<h2>Selecciona una categoría </h2>
-    </div>
-	    
-    		<form action="autenticacion.php" method="post">
-        		<select name="categoria" size="3">
-            			<option value="hardware" selected>Equipamiento y dispositivos</option>
+   
+    		<form action="autenticacion.php" method="post" class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin">
+                <div class="w3-container w3-deep-orange w3-center">
+		              <h2>Selecciona una categoría </h2>
+                </div>
+                
+                <div class="w3-row w3-section w3-center">
+        		<select class="w3-select w3-border" name="categoria">
+                        <option value="" disabled selected>Selecciona una categoría</option>
+            			<option value="hardware">Equipamiento y dispositivos</option>
             			<option value="software">Aplicaciones y herramientas</option>   
             			<option value="red">Comunicaciones y conectividad</option>
         		</select>
+                </div>
+                
+                <div class="w3-container w3-deep-orange w3-center">
+                        <h2>Resumen</h2>
+                </div>
+                
+                <div class="w3-row w3-section w3-center">
+                    <textarea name = resumen rows = 1 cols = 30> ¿Cual es el problema?</textarea> 
+                </div>
+                
+                <div class="w3-container w3-deep-orange w3-center">
+                        <h2>Descripción</h2>
+                </div>
+                
+             
+                <div class="w3-row w3-section w3-center">
+                    <textarea name = descripcion rows = 5 cols = 30> Describe el problema </textarea> 
+                <div class="w3-row w3-section w3-center">
+                    <p><input type = submit value = "Crear" /> <input type = reset value = "Cancelar" /></p> 
+                </div>
+                
+            </form> 
+       
+    </body>    
         
-<p>Descripción (¿Cual es el problema?) </p> 
-<br /> <textarea name = descripcion rows = 5 cols = 1></textarea> 
-
-<p>Resumen (Detalla el problema)</p> 
-<br /> <textarea name = descripcion rows = 5 cols = 10></textarea> 
- 
-<p> <input type = submit value = "Crear" /> <input type = reset value = "Cancelar" /> </p> </form> 
-    
-    
-    
-    
-    <br>
-    
-    <div class="w3-container w3-deep-orange w3-center">
-		<h2>Usuario administrador</h2>
-    </div>
-    
-    <br><br>
-
-    <div class="w3-container w3-center">
-    
-        <a href="crearincidencia.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Nueva incidencia</a>
-   
-    <br><br>
-
-        <a href="consultaincidenciaadm.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Consultar incidencia</a>
         
-    <br><br>
-    
-        <a href="basedeconocimientos.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Base de conocimientos</a>
-   
-    <br><br>
-    
-        <a href="administracionusuarios.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Administracion de usuarios</a>
-        
-
-        
-    </div>
-
-</html>
- 
+    </html>
