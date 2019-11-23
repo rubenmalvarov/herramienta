7a18f6458dc27ad9c0e800b456d87c29afe2e395
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-23 17:54:41 +0100

    Update autenticacion2.php

--- html/autenticacion2.php
+++ html/autenticacion2.php
@@ -10,54 +10,23 @@ session_start();
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 	</head>
 	<body>
 		<div class="container">
 		
 			<?php
-			// Connection info. file
-			include 'conn2.php';	
+			// Creamos las variables con los datos necesarios para el acceso a la BBDD.
 			
-			// Connection variables
-			$conn2 = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-			// Check connection
-			if (!$conn2) {
-				die("Connection failed: " . mysqli_connect_error());
-			}
+				$bdservidor = "localhost";	  	// Nombre del servidor
+				$bdusuario	= "root";		// Usuario de la BBDD
+				$bdcontrasena	= "clase1234";		// Contraseña
+				$bdnombre	= "tfg";    	  	// Nombre de la BBDD
 			
-			// data sent from form login.html 
-			$email = $_POST['email']; 
-			$password = $_POST['password'];
+			// Creamos la conexión con el servidor de la BBDD
+			$conexion = mysqli_connect($bdservidor, $dbusuario, $bdcontrasena ) or die ("No se pudo conectar a la BBDD");
 			
-			// Query sent to database
-			$result = mysqli_query($conn2, "SELECT Email, Password, Name FROM users WHERE Email = '$email'");
-			
-			// Variable $row hold the result of the query
-			$row = mysqli_fetch_assoc($result);
-			
-			// Variable $hash hold the password hash on database
-			$hash = $row['Password'];
-			
-			/* 
-			password_Verify() function verify if the password entered by the user
-			match the password hash on the database. If everything is OK the session
-			is created for one minute. Change 1 on $_SESSION[start] to 5 for a 5 minutes session.
-			*/
-			if (password_verify($_POST['password'], $hash)) {	
-				
-				$_SESSION['loggedin'] = true;
-				$_SESSION['name'] = $row['Name'];
-				$_SESSION['start'] = time();
-				$_SESSION['expire'] = $_SESSION['start'] + (15 * 60) ;						
-				
-		
-			
-			} else {
-				echo "<div class='alert alert-danger mt-4' role='alert'>Email or Password are incorrects!
-				<p><a href='login.html'><strong>Please try again!</strong></a></p></div>";			
-			}	
+			// Ahora hay que conectarse a la BBDD.
+			$bd = mysqli_select_db( $conexion, $bdnombre ) or die ("Esa BBDD no existe compañero");	
 			?>
 		</div>
 
