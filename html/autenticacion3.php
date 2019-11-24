e9c12219fe2f998eb2176fef648cdd647a2eeb83
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-24 20:49:11 +0100

    Update and rename autenticacion3 to autenticacion3.php

--- html/autenticacion3.php
+++ html/autenticacion3.php
@@ -18,15 +18,16 @@ session_start();
 
  <?php 
 			
+		
 			// Creamos las variables con los datos necesarios para el acceso a la BBDD.
 			
-			$bdservidor = "localhost";	  	// Nombre del servidor
+			$bdservidor = "192.168.100.10";	  	// Nombre del servidor
 			$bdusuario	= "root";		// Usuario de la BBDD
 			$bdcontrasena	= "clase1234";		// Contraseña
 			$bdnombre	= "tfg";    	  	// Nombre de la BBDD
 			
 			// Creamos la conexión con el servidor de la BBDD
-			$conexion = mysqli_connect($bdservidor, $dbusuario, $bdcontrasena ) or die ("No se pudo conectar a la BBDD");
+			$conexion = mysqli_connect($bdservidor, $bdusuario, $bdcontrasena ) or die ("No se pudo conectar a la BBDD");
 			
 			// Ahora hay que conectarse a la BBDD.
 			$bd = mysqli_select_db( $conexion, $bdnombre ) or die ("Esa BBDD no existe compañero");		
