0729a8286e5b5a7c2913b452a51dfbbca5b31280
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-26 15:57:59 +0100

    Update formulario1adm.php

--- html/formulario1adm.php
+++ html/formulario1adm.php
@@ -1,12 +1,9 @@
 
-
 <!doctype html>
 <html lang="es">
   <head>
-    <title>NukeTrack Administrador</title>
+    <title>NukeTrack Usuario</title>
 	 <meta charset="UTF-8">
-
-
     <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
 	<link rel="stylesheet" href="css/w3.css">
 	<link rel="stylesheet" href="css/estilo.css">
@@ -16,36 +13,24 @@
     <div class="w3-container w3-center">  
 	   <img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
     </div>
-    
-    <br>
-    
-    <div class="w3-container w3-deep-orange w3-center">
-		<h2>Usuario administrador</h2>
-    </div>
-    
-    <br>
 
-    <div class="w3-container w3-center">
-    
+    <div class="w3-container w3-card-4 w3-light-grey w3-margin w3-center">
+           
+    <div class="w3-container w3-deep-orange w3-center">
+		<h2>Usuario</h2>
+    </div>
+        
+    <br>
+        
         <a href="crearincidencia.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Nueva incidencia</a>
    
     <br><br>
-
-        <a href="consultaincidenciaadm.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Consultar incidencia</a>
         
+        <a href="consultaincidenciausr.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Consultar incidencia</a>
+
     <br><br>
-    
-        <a href="basedeconocimientos.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Base de conocimientos</a>
-   
-    <br><br>
-    
-        <a href="administracionusuarios.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Administracion de usuarios</a>
         
     </div>
-	<footer>
-	<div class="w3-container w3-black">
-		<h4>Álvaro Vicente y Rubén Martín TFG</h4>
-	</div>
-	</footer>
-
+    
+     <a href="index.html" class="w3-button w3-deep-orange w3-round-xlarge" style="width:25%">Cerrar sesión</a>
 </html>
