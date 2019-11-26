755e235a5a6c41e98e9f658b3cf0ed6d1abe96e1
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-26 16:24:17 +0100

    CSS y Enlaces definitivos formulario crear inc

--- html/crearincidencia.php
+++ html/crearincidencia.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="es">
   <head>
-    <title>NukeTrack Administrador</title>
+    <title>NukeTrack Nueva Incidencia</title>
 	 <meta charset="UTF-8">
     <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
 	<link rel="stylesheet" href="css/w3.css">
@@ -15,13 +15,13 @@
     </div>
     
     <!-- Contenedor principal con catégorias y datos de la incidencia a registrar por el usuario -->     
-   
-    		<form action="autenticacion.php" method="post" class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin">
+    <div class="w3-container w3-card-4 w3-light-grey w3-margin">
+    		<form action="autenticacion.php" method="post">
                 <div class="w3-container w3-deep-orange w3-center">
 		              <h2>Selecciona una categoría </h2>
                 </div>
                 
-                <div class="w3-row w3-section w3-center">
+                <div class="w3-row w3-section">
         		<select class="w3-select w3-border" name="categoria">
                         <option value="" disabled selected>Selecciona una categoría</option>
             			<option value="hardware">Equipamiento y dispositivos</option>
@@ -44,13 +44,33 @@
                 
              
                 <div class="w3-row w3-section">
-                    <textarea name = descripcion rows = 5 cols = 30> Describe el problema </textarea> 
-                <div class="w3-row w3-section w3-center">
-                    <p><input type = submit value = "Crear" /> <input type = reset value = "Cancelar" /></p> 
+                    <textarea name = descripcion rows = 5 cols = 30> Describe el problema </textarea>
+                </div>
+                <div class="w3-row w3-section">
+                    <p><input type = submit value = "Crear"class="w3-btn w3-deep-orange w3-round-xlarge" /> 
+                        <input type = reset value = "Cancelar" class="w3-btn w3-deep-orange w3-round-xlarge"/></p> 
                 </div>
                 
-		<a href="formuario1adm.php" class="w3-button w3-deep-orange w3-round-xlarge" style="width:50%">Volver a consola de administrador</a>
-            </form> 
+		      <a href="formuario1adm.php" class="w3-button w3-deep-orange w3-round-xlarge" style="width:50%">Volver a consola de administrador</a>
+             <br>
+                  <br>
+        </form>
+        
+        </div>
+                
+                 <!-- Pie de página y enlace a Login. -->
+    
+        <br>
+        <a href="index.html" class="w3-button w3-deep-orange w3-round-xlarge" style="width:25%">Cerrar sesión</a>
+
+        <br>
+        <br>
+	   <footer>
+	   <div class="w3-container w3-black" style="width:100%">
+		<h4>Álvaro Vicente y Rubén Martín TFG</h4>
+	   </div>
+	   </footer>
+
        
     </body>    
         
