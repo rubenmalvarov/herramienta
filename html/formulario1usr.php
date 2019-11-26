c19cf8e83812e3b6526b24897705ec20809eac6c
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-11-26 15:52:02 +0100

    CSS y enlaces definitivos

--- html/formulario1usr.php
+++ html/formulario1usr.php
@@ -13,20 +13,24 @@
     <div class="w3-container w3-center">  
 	   <img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
     </div>
-    
-    <br>
-    
+
+    <div class="w3-container w3-card-4 w3-light-grey w3-margin w3-center">
+           
     <div class="w3-container w3-deep-orange w3-center">
 		<h2>Usuario</h2>
     </div>
-    
-    <br>
-    <div class="w3-container w3-center">
-    
+        
+    <br><br>
+        
         <a href="crearincidencia.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Nueva incidencia</a>
    
     <br><br>
+        
         <a href="consultaincidenciausr.php" class="w3-button w3-deep-orange w3-center w3-round-xlarge" style="width:50%">Consultar incidencia</a>
+
+    <br><br>
         
     </div>
+    
+     <a href="index.html" class="w3-button w3-deep-orange w3-round-xlarge" style="width:25%">Cerrar sesión</a>
 </html>
