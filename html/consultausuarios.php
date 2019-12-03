1283469d6db33172405aaf79f5259d0fc9624e17
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-12-03 17:55:16 +0100

    Falta crear la validación "consultausuarios1.php"

---
+++ html/consultausuarios.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>NukeTrack Administrador</title>
+	 <meta charset="UTF-8">
+    <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
+	<link rel="stylesheet" href="css/w3.css">
+	<link rel="stylesheet" href="css/estilo.css">
+ </head>
+   
+    <!-- Imagen y logo -->
+    <div class="w3-container w3-center">  
+	   <img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
+    </div>
+    
+    <br>
+ 
+        
+
+    
+    <div class="w3-container w3-deep-orange w3-center">
+		<h2>Introduce el nombre de usuario sobre el que quiere realziar la consulta</h2>
+	</div>
+
+	<form class="w3-container" action="consultausuarios1.php" method="post">
+		<p>
+			<label class="w3-label">
+				Usuario
+			</label>
+			<input class="w3-input w3-border" type="text"placeholder=“email” name="email">
+		</p>
+	</form>
+          
+       
+        </div>
+        
+        <br>
+        
+        
+         <br>
+        <br>
+    </div>
+  <a href="formuario1adm.php" class="w3-button w3-deep-orange w3-round-xlarge" style="width:50%">Volver a consola de administrador</a>
+     <br>
+    
+  
+    <!-- Pie de página y enlace a Login. -->
+    
+    <br>
+    <a href="index.html" class="w3-button w3-deep-orange w3-round-xlarge" style="width:25%">Cerrar sesión</a>
+    <br>
+    <br>
+	<footer>
+	<div class="w3-container w3-black" style="width:100%">
+		<h4>Álvaro Vicente y Rubén Martín TFG</h4>
+	</div>
+	</footer>
+    
+</html>
