1d646e507c12ba51c336091516edae3707a38bf1
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-12-03 10:23:37 +0100

    No es necesario se usa conexion

--- html/variables.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Connection variables
-$dbhost	= "localhost";	   // IP del servidor
-$dbidusuario	= "root";		           // Nombre de usuario de la bbdd
-$dbcontrasena	= "clase1234";		     // contraseña de dicho usuario
-$dbnombre	= "tfg";                   // Nombre de la BBDD
-?>
