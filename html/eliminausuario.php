8e47a3ec68acc4018363d8aba4cb5b3a4612eeee
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-12-07 20:14:42 +0100

    Update eliminausuario.php

--- html/eliminausuario.php
+++ html/eliminausuario.php
@@ -1,19 +1,15 @@
 <!doctype html>
 <html lang="es">
-  <head>
-    <title>NukeTrack Nueva Incidencia</title>
+	  <head>
+    <title>NukeTrack Acceso</title>
 	 <meta charset="UTF-8">
+
+
     <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
 	<link rel="stylesheet" href="css/w3.css">
 	<link rel="stylesheet" href="css/estilo.css">
- </head>
-    <body>
-   
-    <!-- Imagen y logo -->
-    <div class="w3-container w3-center">  
-	   <img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
-    </div>
-
+  </head>
+	<body>
 <?php
 			// Creamos las variables con los datos necesarios para el acceso a la BBDD.
 			
@@ -36,32 +32,16 @@
             echo "<form action='borrausuario.php' method='post'>";
             echo "<div class='w3-row w3-section'>";
             echo "<select class='w3-select w3-border' name='categoria'>";
-            echo "<option value="" disabled selected>Seleccione el usuario a eliminar</option>";
 			while ($fila = mysqli_fetch_array( $resultado ))
 			{
                 $id=$fila[idusuario];
                 $nom=$fila[nombre];
                 $ape=$fila[ape1];
-                echo "<option value=$id>$nom $ape</option>";
+                echo "<option value=$id>.$nom.' '.$ape.</option>";
 			}
-	        echo "</select>";
-            echo "<input type = submit value = 'Eliminar' class='w3-btn w3-deep-orange w3-round-xlarge'>"; 
-            echo "</div>";
-            echo "</form>";
-?>
-
-
-  <br>
-        <a href="index.html" class="w3-button w3-deep-orange w3-round-xlarge" style="width:25%">Cerrar sesión</a>
-        <br>
-        <br>
-	   <footer>
-	   <div class="w3-container w3-black" style="width:100%">
-		<h4>Álvaro Vicente y Rubén Martín TFG</h4>
-	   </div>
-	   </footer>
-       
-    </body>    
-        
-        
-    </html
+	       	echo "</select>";
+            	echo "</div>";
+           	echo "</form>";
+			?>
+	</body>
+</html>
