723ebaffd2cc6039c5c69629736658c8be5b81ac
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-12-07 16:41:15 +0100

    Update creausuario.php

--- html/creausuario.php
+++ html/creausuario.php
@@ -18,47 +18,62 @@
     <div class="w3-container w3-card-4 w3-light-grey w3-margin">
 	    
     		<form action="nuevousuario.php" method="post">
-                <div class="w3-container w3-deep-orange">
-                       <h2>Introduce nombre</h2>
+                
+            <div class="w3-container w3-deep-orange">
+			<label class="w3-label">
+                     	Introduce nombre
+			</label>
                 </div>
 			
 		<div class="w3-row w3-section">
-                    <textarea name = "nombre" rows = 1 cols = 15> Describe el problema </textarea>
+                    <input class="w3-input w3-border" type="text"placeholder=“nombre” name="nombre">
                 </div>
 		
 		<div class="w3-container w3-deep-orange">
-                       <h2>Introduce primer apellido</h2>
+			<label class="w3-label">
+                     	Introduce primer apellido
+			</label>
                 </div>
 			
-		<div class="w3-row w3-section">
-                    <textarea name = "apellido1" rows = 1 cols = 15> Describe el problema </textarea>
+	<div class="w3-row w3-section">
+                    <input class="w3-input w3-border" type="text"placeholder=“apellido1” name="apellido1">
                 </div>
 			
 		<div class="w3-container w3-deep-orange">
-                       <h2>Introduce Segundo apellido</h2>
+			<label class="w3-label">
+                     	Introduce segundo apellido
+			</label>
                 </div>
 			
-		<div class="w3-row w3-section">
-                    <textarea name = "apellido2" rows = 1 cols = 15> Describe el problema </textarea>
+	<div class="w3-row w3-section">
+                    <input class="w3-input w3-border" type="text"placeholder=“apellido2” name="apellido2">
                 </div>
 			
-                <div class="w3-container w3-deep-orange">
-                        <h2>Introduce email</h2>
+               <div class="w3-container w3-deep-orange">
+			<label class="w3-label">
+                     	Introduce email
+			</label>
                 </div>
                 
-                <div class="w3-row w3-section">
-                    <textarea name = "email" rows = 1 cols = 30> ¿Cual es el problema?</textarea> 
+              <div class="w3-row w3-section">
+                    <input class="w3-input w3-border" type="text"placeholder=“email” name="email">
                 </div>
                 
-                <div class="w3-container w3-deep-orange">
-                        <h2>Introduce contraseña</h2>
+               <div class="w3-container w3-deep-orange">
+			<label class="w3-label">
+                     	Introduce contraseña
+			</label>
                 </div>
              
                 <div class="w3-row w3-section">
-                    <textarea name = "contrasena" rows = 1 cols = 15> Describe el problema </textarea>
+                    <input class="w3-input w3-border" type="password" placeholder=“contrasena” name="contrasena">
                 </div>
-		
 			
+		<div class="w3-container w3-deep-orange">
+			<label class="w3-label">
+                     	Selecciona tipo de usuario
+			</label>
+                </div>	
 		<div class="w3-row w3-section">
         		<select class="w3-select w3-border" name="permisos">
                         <option value="" disabled selected>Selecciona un tipo de usuario</option>
