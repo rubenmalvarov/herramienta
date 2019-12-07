c154698ab269e1c7c9a677635b49e18f6ed63fb0
rubenmalvarov <56297745+rubenmalvarov@users.noreply.github.com>
2019-12-07 19:20:28 +0100

    Update eliminausuario.php

--- html/eliminausuario.php
+++ html/eliminausuario.php
@@ -1,21 +1,4 @@
-<!doctype html>
-<html lang="es">
-  <head>
-    <title>NukeTrack Nuevo usuario</title>
-	 <meta charset="UTF-8">
-    <!-- Aqui acceso al framework de W3School y a nuestro propio CSS -->
-	<link rel="stylesheet" href="css/w3.css">
-	<link rel="stylesheet" href="css/estilo.css">
- </head>
-    <body>
-   
-    <!-- Imagen y logo -->
-    <div class="w3-container w3-center">  
-	   <img src="imagenes/logo.png" class="w3-circle" alt="NukeTrack Logo">
-    </div>
-    
-    <!-- Contenedor principal con catégorias y datos de la incidencia a registrar por el usuario -->
-    <?php
+<?php
 			// Creamos las variables con los datos necesarios para el acceso a la BBDD.
 			
 			$bdservidor = "192.168.100.10";	  	// Nombre del servidor
@@ -30,51 +13,21 @@
 			$bd = mysqli_select_db( $conexion, $bdnombre ) or die ("Esa BBDD no existe compañero");	
 			
 			//Probamos con una consulta sencillita
-			$consulta = "SELECT idusuario, nombre, ape1, email FROM usuarios";
+			$consulta = "SELECT idusuario, nombre, ape1 FROM usuarios";
 			$resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
 			
 			// Y la mostramos en PHP
-			echo "<div class='w3-container w3-card-4 w3-light-grey w3-margin'>";
-			echo "<form action='nuevousuario.php' method='post'>";
-			echo "<div class='w3-row w3-section''>";
-			echo "<select class='w3-select w3-border'' name='permisos''>";
-			while ($columna = mysqli_fetch_array( $resultado ))
+            echo "<form action='nuevainc.php' method='post'>";
+            echo "<div class='w3-row w3-section'>";
+            echo "<select class='w3-select w3-border' name='categoria'>";
+			while ($fila = mysqli_fetch_array( $resultado ))
 			{
-				echo "<option value=$columna['idusuario']>. $columna['nombre'] . $columna['ape1'] .</option>";
+                $id=$fila[idusuario];
+                $nom=$fila[nombre];
+                $ape=$fila[ape1];
+                echo "<option value=$id>.$nom.' '.$ape.</option>";
 			}
-			echo "</select>";
-            echo "</div>";
-            echo "</form>";
-    ?>
-        
-        
-    <div class="w3-container w3-card-4 w3-light-grey w3-margin">
-	   
-			
-                <div class="w3-row w3-section">
-                    <p><input type = submit value = "Crear"class="w3-btn w3-deep-orange w3-round-xlarge" /> 
-                </div>
-                
-		    <!-- <a href="formuario1adm.php" class="w3-button w3-deep-orange w3-round-xlarge" style="width:50%">Volver a consola de administrador</a> -->
-             <br>
-                  <br>
-       
-        
-        </div>
-                
-                 <!-- Pie de página y enlace a Login. -->
-    
-        <br>
-        <a href="index.html" class="w3-button w3-deep-orange w3-round-xlarge" style="width:25%">Cerrar sesión</a>
-        <br>
-        <br>
-	   <footer>
-	   <div class="w3-container w3-black" style="width:100%">
-		<h4>Álvaro Vicente y Rubén Martín TFG</h4>
-	   </div>
-	   </footer>
-       
-    </body>    
-        
-        
-    </html>
+	       	echo "</select>";
+            	echo "</div>";
+           	echo "</form>";
+			?>
